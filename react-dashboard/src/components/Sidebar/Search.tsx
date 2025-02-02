74a5521cfb261940a979ee0ab2b5eae6c1e25b2d
Evans <evansmumo665@gmail.com>
2025-02-02 16:37:12 +0300

    sidebar

--- react-dashboard/src/components/Sidebar/Search.tsx
+++ react-dashboard/src/components/Sidebar/Search.tsx
@@ -1,9 +1,13 @@
+"use client"
+
 import React from "react";
+import { useState } from "react";
 import { FiCommand, FiSearch } from "react-icons/fi";
 import { CommandMenu } from "./CommandMenu";
 
 
 const Search = () => {
+  const [open, setOpen] = useState(false)
   return <>
   <div className="bg-stone-200 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm">
     < FiSearch className="mr-2" />
@@ -12,6 +16,7 @@ const Search = () => {
           <FiCommand />K
     </span>
   </div>
+  <CommandMenu open={open} setOpen={setOpen} />
   </>
 }
 
