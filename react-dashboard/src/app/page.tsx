1ab64ff1d5780884ddcd1a5c7e32076c3eccee19
Evans Mumo <evansmumo665@gmail.com>
2025-02-02 12:14:45 +0300

    first commmit

---
+++ react-dashboard/src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sidebar from "@/components/Sidebar/Sidebar";
+import Dashboard from "@/components/Dashboard/Dashboard";
+
+export default function Home() {
+  return (
+    <main className="grid gap-4 p-4 grid-cols-[220px,_1fr]">
+      <Sidebar />
+      <Dashboard />
+    </main>
+  );
+}
